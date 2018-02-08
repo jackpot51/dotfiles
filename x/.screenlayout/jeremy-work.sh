b8ce43d92c7a28875e6887b6e41b0675678c57f7
Jeremy Soller <jeremy@system76.com>
2018-02-08 09:17:32 -0700

    Add work xrandr script

---
+++ x/.screenlayout/jeremy-work.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-1 --off --output DisplayPort-0 --off --output DVI-D-0 --mode 2560x1440 --pos 2560x0 --rotate normal --output HDMI-A-1 --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-A-0 --off

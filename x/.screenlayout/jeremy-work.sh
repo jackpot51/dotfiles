6200d15bc74cb51b3479b2486165cf0ae846e8d1
Jeremy Soller <jeremy@system76.com>
2019-09-26 12:22:20 -0600

    Update jeremy-work display config

--- x/.screenlayout/jeremy-work.sh
+++ x/.screenlayout/jeremy-work.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-1 --mode 2560x1440 --pos 2560x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-3 --off --output DisplayPort-2 --mode 2560x1440 --pos 0x0 --rotate normal --output DVI-D-0 --off --output HDMI-A-1 --off --output HDMI-A-0 --mode 2560x1440 --pos 5120x0 --rotate normal
+xrandr --output DisplayPort-1 --mode 2560x1440 --pos 2560x0 --rotate normal --output DisplayPort-0 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DVI-D-0 --off --output HDMI-A-1 --off --output HDMI-A-0 --mode 2560x1440 --pos 5120x0 --rotate normal

d7a2e8b284d68453abf925685e1a2922d2509651
Jeremy Soller <jeremy@system76.com>
2020-04-19 08:33:05 -0600

    Update jeremy-desktop screen layout

--- x/.screenlayout/jeremy-desktop.sh
+++ x/.screenlayout/jeremy-desktop.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-D-0 --off --output HDMI-0 --off --output DP-0 --mode 2560x1440 --pos 5120x0 --rotate normal --output DP-1 --off --output DP-2 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-3 --off --output DP-4 --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-5 --off
+xrandr --output DP-0 --primary --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-1 --off --output HDMI-0 --off --output DP-2 --mode 2560x1440 --pos 5120x0 --rotate normal --output DP-3 --off --output DP-4 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-5 --off --output USB-C-0 --off

9d0c6b30d62a888e2924f8463c6f569aaead151c
Jeremy Soller <jeremy@system76.com>
2021-12-22 09:58:27 -0700

    Install alacritty in silverblue

--- silverblue.sh
+++ silverblue.sh
@@ -2,9 +2,19 @@
 
 set -x
 
+if ! command -v alacritty
+then
+	rpm-ostree install alacritty
+fi
+
 if [ ! -d /usr/share/fonts/ibm-plex-mono-fonts/ ]
 then
 	rpm-ostree install ibm-plex-mono-fonts
+fi
+
+if ! command -v xprop
+then
+	rpm-ostree install xprop
 fi
 
 toolbox run stow bash

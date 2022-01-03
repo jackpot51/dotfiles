2391a8e698bb34b26b3a1c16436dbc55c176fb6e
Jeremy Soller <jeremy@system76.com>
2022-01-03 14:24:38 -0700

    Save/load GPG

--- secret/load.sh
+++ secret/load.sh
@@ -2,8 +2,12 @@
 
 set -ex
 
+gpg --import secret/gpg.asc
 tar \
 	--extract \
 	--verbose \
 	--file "secret/${HOSTNAME}.tar" \
 	--directory="$HOME"
+
+rm -f secret/gpg.asc
+rm -f "secret/${HOSTNAME}.tar"
--- secret/save.sh
+++ secret/save.sh
@@ -2,6 +2,13 @@
 
 set -ex
 
+rm -f secret/gpg.asc
+rm -f "secret/${HOSTNAME}.tar"
+
+gpg --output secret/gpg.asc --export-secret-keys --armor
+
+touch "secret/${HOSTNAME}.tar"
+chmod 600 "secret/${HOSTNAME}.tar"
 tar \
 	--create \
 	--verbose \

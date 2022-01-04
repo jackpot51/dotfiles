daf73b3012f9679d427e1b38a1dfbcad5accec5e
Jeremy Soller <jeremy@system76.com>
2022-01-03 21:07:57 -0700

    Set all packages except those desired as automatic

--- scripts/popos.sh
+++ scripts/popos.sh
@@ -2,15 +2,38 @@
 
 set -ex
 
-if [ ! -d /usr/share/fonts/truetype/ibm-plex/ ]
-then
-	sudo apt-get install fonts-ibm-plex
-fi
+APTPKGS=(
+cifs-utils
+fonts-ibm-plex
+git-lfs
+iotop
+kpartx
+kpartx-boot
+nethogs
+pop-desktop
+software-properties-common
+steam
+system76-driver
+vim-gtk3
+)
 
-if [ ! -f /usr/bin/vim.gtk3 ]
-then
-	sudo apt-get install vim-gtk3
-fi
+LANGUAGES=(
+en_US
+zh_CN
+)
+
+# Mark all packages as automatically installed
+sudo apt-mark auto $(apt-mark showmanual)
+
+# Mark desired packages as manually installed
+sudo apt-get install "${APTPKGS[@]}"
+
+# Install language packages
+for language in "${LANGUAGES[@]}"
+do
+	sudo apt-get install \
+		$(check-language-support --show-installed --language="$language")
+done
 
 . scripts/common.sh
 
